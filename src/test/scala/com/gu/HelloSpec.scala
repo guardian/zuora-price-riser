d2d792580323a3911921f546b613e86c3c84d157
Mario Galic <mario.galic@guardian.co.uk>
2018-11-30 14:36:18 +0000

    Add tests for PeriodContainsDate

--- src/test/scala/com/gu/HelloSpec.scala
+++
@@ -1,9 +0,0 @@
-package com.gu
-
-import org.scalatest._
-
-class MainSpec extends FlatSpec with Matchers {
-  "The Main object" should "satisfy criteria" in {
-    true shouldEqual true
-  }
-}
---
+++ src/test/scala/com/gu/PeriodContainsDateSpec.scala
@@ -0,0 +1,38 @@
+package com.gu
+
+import java.time.LocalDate
+
+import org.scalatest._
+import java.time.format.DateTimeFormatter
+
+class PeriodContainsDateSpec extends FlatSpec with Matchers {
+  val zuoraDateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+
+  "PeriodContainsDate" should "include the start bound" in {
+    val start = LocalDate.parse("2018-09-01", zuoraDateFormat)
+    val end = LocalDate.parse("2018-12-01", zuoraDateFormat)
+    val date = LocalDate.parse("2018-09-01", zuoraDateFormat)
+    PeriodContainsDate(start, end, date) should be(true)
+  }
+
+  it should "exclude the end bound" in {
+    val start = LocalDate.parse("2018-09-01", zuoraDateFormat)
+    val end = LocalDate.parse("2018-12-01", zuoraDateFormat)
+    val date = LocalDate.parse("2018-12-01", zuoraDateFormat)
+    PeriodContainsDate(start, end, date) should be(false)
+  }
+
+  it should "not contain dates before the start of the period" in {
+    val start = LocalDate.parse("2018-09-01", zuoraDateFormat)
+    val end = LocalDate.parse("2018-12-01", zuoraDateFormat)
+    val date = LocalDate.parse("2018-08-01", zuoraDateFormat)
+    PeriodContainsDate(start, end, date) should be(false)
+  }
+
+  it should "not contain dates after the end of the period" in {
+    val start = LocalDate.parse("2018-09-01", zuoraDateFormat)
+    val end = LocalDate.parse("2018-12-01", zuoraDateFormat)
+    val date = LocalDate.parse("2018-12-02", zuoraDateFormat)
+    PeriodContainsDate(start, end, date) should be(false)
+  }
+}
