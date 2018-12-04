c4a433c3980459a9a09dd5f1a964008f606d5cff
Mario Galic <mario.galic@guardian.co.uk>
2018-12-04 11:11:04 +0000

    Inline CurrentGuardianWeeklyRatePlanPredicate objects

--- src/main/scala/com/gu/CurrentGuardianWeeklySubscription.scala
+++ src/main/scala/com/gu/CurrentGuardianWeeklySubscription.scala
@@ -1,20 +1,69 @@
 package com.gu
 
 import org.joda.time.LocalDate
+import scala.util.Try
 
 /**
-  * Conditions defining what Guardian Weekly subscription the customer has today
+  * Conditions defining what Guardian Weekly subscription the customer has today.
   */
-sealed trait CurrentGuardianWeeklyRatePlanConditions
-case object RatePlanIsGuardianWeekly extends CurrentGuardianWeeklyRatePlanConditions
-case object RatePlanHasNotBeenRemoved extends CurrentGuardianWeeklyRatePlanConditions
-case object TodayHasBeenInvoiced extends CurrentGuardianWeeklyRatePlanConditions // FIXME: Invoiced raised today but after running the script?
-case object RatePlanHasACharge extends CurrentGuardianWeeklyRatePlanConditions
-case object RatePlanHasOnlyOneCharge extends CurrentGuardianWeeklyRatePlanConditions
-case object ChargeIsQuarterlyOrAnnual extends CurrentGuardianWeeklyRatePlanConditions
+sealed trait CurrentGuardianWeeklyRatePlanPredicate extends (RatePlan => Boolean)
+case object RatePlanIsGuardianWeekly extends CurrentGuardianWeeklyRatePlanPredicate {
+  def apply(ratePlan: RatePlan): Boolean = Config.Zuora.guardianWeeklyProductRatePlanIds.contains(ratePlan.productRatePlanId)
+}
+case object RatePlanHasNotBeenRemoved extends CurrentGuardianWeeklyRatePlanPredicate {
+  def apply(ratePlan: RatePlan): Boolean =
+    ratePlan.lastChangeType.isEmpty || !ratePlan.lastChangeType.contains("Remove")
+}
+case object TodayHasBeenInvoiced extends CurrentGuardianWeeklyRatePlanPredicate { // FIXME: Invoiced raised today but after running the script?
+  def apply(ratePlan: RatePlan): Boolean =
+    Try {
+      PeriodContainsDate(
+        startPeriodInclusive = ratePlan.ratePlanCharges.head.processedThroughDate.get,
+        endPeriodExcluding = ratePlan.ratePlanCharges.head.chargedThroughDate.get,
+        date = LocalDate.now()
+      )
+    }.getOrElse(false)
+}
+case object RatePlanHasACharge extends CurrentGuardianWeeklyRatePlanPredicate {
+  def apply(ratePlan: RatePlan): Boolean = ratePlan.ratePlanCharges.nonEmpty
+}
+case object RatePlanHasOnlyOneCharge extends CurrentGuardianWeeklyRatePlanPredicate {
+  def apply(ratePlan: RatePlan): Boolean = ratePlan.ratePlanCharges.size == 1
+}
+case object ChargeIsQuarterlyOrAnnual extends CurrentGuardianWeeklyRatePlanPredicate {
+  def apply(ratePlan: RatePlan): Boolean =
+    Try(List("Annual", "Quarterly").contains(ratePlan.ratePlanCharges.head.billingPeriod)).getOrElse(false)
+}
+
+
+/**
+  * What Guardian Weekly does the customer have today?
+  *
+  * Zuora subscription can have multiple rate plans so this function selects just the one representing
+  * current Guardian Weekly subscription. Given a Zuora subscription return a single current rate plan
+  * attached to Guardian Weekly product that satisfies all of the CurrentGuardianWeeklyRatePlanPredicates.
+  *
+  * @return current RatePlan representing Guardian Weekly
+  */
+object CurrentGuardianWeeklyRatePlan extends (Subscription => Option[RatePlan]) {
+  def apply(subscription: Subscription): Option[RatePlan] =
+    subscription.ratePlans.find { ratePlan =>
+      List[CurrentGuardianWeeklyRatePlanPredicate](
+        RatePlanIsGuardianWeekly,
+        RatePlanHasNotBeenRemoved,
+        RatePlanHasACharge,
+        RatePlanHasOnlyOneCharge,
+        TodayHasBeenInvoiced,
+        ChargeIsQuarterlyOrAnnual
+      ).forall(P => P(ratePlan))
+    }
+}
 
 /**
   * Model representing 'What Guardian Weekly does the customer have today?'
+  *
+  * The idea is to have a single unified object as an answer to this question because Zuora's answer is
+  * scattered across multiple objects such as Subscription, RatePlan, RatePlanCharge.
   *
   * FIXME: override flag?
   */
@@ -47,55 +96,25 @@ case class CurrentInvoicedPeriod(
 }
 
 /**
-  * What Guardian Weekly does the customer have today?
-  *
-  * Zuora subscription can have multiple rate plans so this function selects just the one representing
-  * current Guardian Weekly subscription. Given a Zuora subscription and account return a single current rate plan
-  * attached to Guardian Weekly product that satisfies all of the CurrentGuardianWeeklyRatePlanConditions.
-  *
-  * @return CurrentGuardianWeeklySubscription current rate plan attached to a Guardian Weekly product
+  * Given a subscription and account Zuora objects it constructs CurrentGuardianWeeklySubscription representing
+  * and unified view of what Guardian Weekly customer has today in a single object.
   */
 object CurrentGuardianWeeklySubscription extends ((Subscription, Account) => CurrentGuardianWeeklySubscription) {
-  def apply(subscription: Subscription, account: Account): CurrentGuardianWeeklySubscription = {
-    val currentRatePlans = subscription.ratePlans.filter { ratePlan =>
-      List[(CurrentGuardianWeeklyRatePlanConditions, Boolean)](
-        RatePlanIsGuardianWeekly -> IsGuardianWeeklyProductRatePlanId(ratePlan),
-        RatePlanHasNotBeenRemoved -> (ratePlan.lastChangeType.isEmpty || !ratePlan.lastChangeType.contains("Remove")),
-        RatePlanHasACharge -> ratePlan.ratePlanCharges.nonEmpty,
-        RatePlanHasOnlyOneCharge -> (ratePlan.ratePlanCharges.size == 1),
-        TodayHasBeenInvoiced -> todayIsWithinInvoicedPeriod(ratePlan.ratePlanCharges.head),
-        ChargeIsQuarterlyOrAnnual -> List("Annual", "Quarterly").contains(ratePlan.ratePlanCharges.head.billingPeriod)
-      ).forall(_._2 == true)
-    }
+  def apply(subscription: Subscription, account: Account): CurrentGuardianWeeklySubscription =
+    CurrentGuardianWeeklyRatePlan(subscription).map { currentGuardianWeeklyRatePlan =>
+      assert(currentGuardianWeeklyRatePlan.ratePlanCharges.size == 1, "RatePlanHasOnlyOneCharge not satisfied")
+      assert(currentGuardianWeeklyRatePlan.ratePlanCharges.head.processedThroughDate.isDefined, "TodayHasBeenInvoiced not satisfied")
 
-    assert(currentRatePlans.size == 1, s"There should be exactly one current plan: $subscription") // FIXME: Can this be handled?
-
-    val currentRatePlan = currentRatePlans.head
-    val currentRatePlanCharge = currentRatePlan.ratePlanCharges.head
-
-    CurrentGuardianWeeklySubscription(
-      subscriptionNumber = subscription.subscriptionNumber,
-      billingPeriod = currentRatePlanCharge.billingPeriod,
-      price = currentRatePlanCharge.price,
-      currency = account.billingAndPayment.currency,
-      country = account.soldToContact.country,
-      invoicedPeriod = CurrentInvoicedPeriod(
-        startDateIncluding = currentRatePlanCharge.processedThroughDate.get,
-        endDateExcluding = currentRatePlanCharge.chargedThroughDate.get)
-    )
-  }
-
-  // Invoiced period = [processedThroughDate, chargedThroughDate)
-  private def todayIsWithinInvoicedPeriod(ratePlanCharge: RatePlanCharge): Boolean =
-    if (notInvoiced(ratePlanCharge))
-      false
-    else
-      PeriodContainsDate(
-        startPeriodInclusive = ratePlanCharge.processedThroughDate.get,
-        endPeriodExcluding = ratePlanCharge.chargedThroughDate.get,
-        date = LocalDate.now()
+      CurrentGuardianWeeklySubscription(
+        subscriptionNumber = subscription.subscriptionNumber,
+        billingPeriod = currentGuardianWeeklyRatePlan.ratePlanCharges.head.billingPeriod,
+        price = currentGuardianWeeklyRatePlan.ratePlanCharges.head.price,
+        currency = account.billingAndPayment.currency,
+        country = account.soldToContact.country,
+        invoicedPeriod = CurrentInvoicedPeriod(
+          startDateIncluding = currentGuardianWeeklyRatePlan.ratePlanCharges.head.processedThroughDate.get,
+          endDateExcluding = currentGuardianWeeklyRatePlan.ratePlanCharges.head.chargedThroughDate.get)
       )
-
-  private def notInvoiced(ratePlanCharge: RatePlanCharge): Boolean =
-    ratePlanCharge.processedThroughDate.isEmpty || ratePlanCharge.chargedThroughDate.isEmpty
+    }.getOrElse(throw new AssertionError(s"Subscription does not have a current Guardian Weekly rate plan: $subscription; $account"))
 }
+
