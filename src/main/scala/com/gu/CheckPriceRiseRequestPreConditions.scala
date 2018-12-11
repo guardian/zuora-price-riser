3989c0377d71504e9ca947765956e5971d22bc4f
Mario Galic <mario.galic@guardian.co.uk>
2018-12-11 17:12:30 +0000

    Multiple rate plans can be removed

--- src/main/scala/com/gu/CheckPriceRiseRequestPreConditions.scala
+++ src/main/scala/com/gu/CheckPriceRiseRequestPreConditions.scala
@@ -3,9 +3,8 @@ package com.gu
 trait PriceRiseRequestPreCondition
 case object OldRatePlanShouldBeRemovedOnTheSameDateAsTheNewRatePlanIsAdded extends PriceRiseRequestPreCondition
 case object PriceIsWithinReasonableBounds extends PriceRiseRequestPreCondition
-case object SingleRatePlanIsRemoved extends PriceRiseRequestPreCondition
 case object SingleRatePlanIsAdded extends PriceRiseRequestPreCondition
-case object HolidayIsNotRemoved extends PriceRiseRequestPreCondition
+case object HolidayAndRetentionDiscountRatePlansShouldNotBeRemoved extends PriceRiseRequestPreCondition
 
 /**
   * Checks pre-conditions on the Zuora API request body before serialization to JSON.
@@ -21,9 +20,8 @@ object CheckPriceRiseRequestPreConditions {
     val (_, unsatisfied) = List[(PriceRiseRequestPreCondition, Boolean)](
       OldRatePlanShouldBeRemovedOnTheSameDateAsTheNewRatePlanIsAdded -> (request.remove.head.contractEffectiveDate == request.add.head.contractEffectiveDate),
       PriceIsWithinReasonableBounds -> (request.add.head.chargeOverrides.get.head.price > currentGuardianWeeklySubscription.price),
-      SingleRatePlanIsRemoved -> (request.remove.size == 1),
       SingleRatePlanIsAdded -> (request.add.size == 1),
-      HolidayIsNotRemoved -> true // TODO
+      HolidayAndRetentionDiscountRatePlansShouldNotBeRemoved -> true, // TODO: Implement HolidayAndRetentionDiscountRatePlansShouldNotBeRemoved - request.remove.map(_.) Config.Zuora.doNotRemoveProductRatePlanIds.contains
     ).partition(_._2)
 
     unsatisfied.map(_._1)
