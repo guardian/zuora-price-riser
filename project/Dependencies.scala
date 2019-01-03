30a862e7d9f251157f52222b421b07fa6194acd3
Mario Galic <mario.galic@guardian.co.uk>
2019-01-03 11:32:27 +0000

    Use cleaner scalactic tolerance syntax

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,6 +2,7 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
+  lazy val scalatic = "org.scalactic" %% "scalactic" % "3.0.5"
   lazy val csvJoda = "com.nrinaudo" %% "kantan.csv-joda-time" % "0.5.0"
   lazy val csvGeneric = "com.nrinaudo" %% "kantan.csv-generic" % "0.5.0"
   lazy val scalajHttp = "org.scalaj" %% "scalaj-http" % "2.4.1"
