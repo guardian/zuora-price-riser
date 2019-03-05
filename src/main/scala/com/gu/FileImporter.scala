7e64241c3c5142d4ba03746c1e7e6e046607c206
Jacob Winch <jacob.winch@guardian.co.uk>
2019-03-05 12:11:21 +0000

    Write full records as part of logging output

--- src/main/scala/com/gu/FileImporter.scala
+++ src/main/scala/com/gu/FileImporter.scala
@@ -15,7 +15,7 @@ object FileImporter {
 
   type subscription_name = String
   type campaign_name = String
-  type date_letter_sent = String // we do not care about this so no LocalDate
+  type date_letter_sent = LocalDate
   type price_rise_date = LocalDate
   type current_price_today = Float
   type guardian_weekly_new_price = Float
@@ -29,7 +29,9 @@ object FileImporter {
     _unsafeCurrentPrice: current_price_today, // WARNING: Do not trust this value.
     newPrice: guardian_weekly_new_price,
     termEndDate: term_end_date
-  )
+  ) {
+    def csvRow(autoRenew: Boolean) = s"$subscriptionName,$campaignName,$dateLetterSent,$priceRiseDate,${_unsafeCurrentPrice},$newPrice,${termEndDate.getOrElse("")},${autoRenew}"
+  }
 
   def importCsv(filename: String = "subs.csv"): List[ReadResult[PriceRise]] =
     new File(filename).asCsvReader[PriceRise](rfc.withHeader).toList
