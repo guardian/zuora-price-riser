2c2108fadd4f62767b7ff14fd9f40e23c545f6b7
Jacob Winch <jacob.winch@guardian.co.uk>
2019-03-05 12:41:38 +0000

    Add logging if the supplied price rise date is invalid

--- src/main/scala/com/gu/PriceRiseFallsOnAcceptableDay.scala
+++ src/main/scala/com/gu/PriceRiseFallsOnAcceptableDay.scala
@@ -1,11 +1,15 @@
 package com.gu
 
+import com.typesafe.scalalogging.LazyLogging
 import org.joda.time.LocalDate
 
-object PriceRiseFallsOnAcceptableDay {
+object PriceRiseFallsOnAcceptableDay extends LazyLogging {
 
   def apply(priceRiseDate: LocalDate, projectedInvoiceItems: List[InvoiceItem]) = {
-    projectedInvoiceItems.map(_.serviceStartDate).contains(priceRiseDate)
+    val acceptablePriceRiseDates = projectedInvoiceItems.map(_.serviceStartDate)
+    val dateIsAcceptable = acceptablePriceRiseDates.contains(priceRiseDate)
+    if (!dateIsAcceptable) logger.warn(s"Price rise date in input file ($priceRiseDate) is unacceptable. Acceptable dates are: $acceptablePriceRiseDates")
+    dateIsAcceptable
   }
 
 }
